72ac04125755b5b5555a9487e0e7a56543a194cf
哇哈哈哈 <wahahaha@MacBook-Pro.local>
2024-08-28 16:06:56 -0400

    update weather detailscontainer,switch tempc doc to it

--- src/components/WeatherDetailsContainer/WeatherDetailsContainer.jsx
+++ src/components/WeatherDetailsContainer/WeatherDetailsContainer.jsx
@@ -1,18 +1,32 @@
 import "./WeatherDetailsContainer.scss";
-import TempC from "../../components/TempC/TempC";
+//import TempC from "../../components/TempC/TempC";
 import sunny from "../../assets/images/sunny.webp";
 
 const WeatherDetailsContainer = ({ data }) => {
+
+const description = data.current.weather[0].description;
+const feelsLike = data.current.feels_like ;
+const uvi = data.current.uvi;
+const icon = data.current.weather.mian;
+let temperature;
+
+if (data) {
+  temperature = data.current.temp;
+} else {
+  temperature = "N/A"; 
+}
+
   return (
     <div className="weather-details-container">
-      <img src={sunny} alt="icon" className="weather-details-container__icon" />
-      <TempC className={"tempc"} data={data} />
+        <img src={sunny} alt="icon" className="weather-details-container__icon" />
+        <span className="weather__temp">{temperature}</span> 
+        <span className="weather__tempC-degree">°F</span>
       <div>
         <h3 className="weather-details-container__weather">
-          {data.current.weather[0].description}
+          {description}
         </h3>
-        <p className="weather-details-container__feels">Feels like 13</p>
-        <p className="weather-details-container__high-low">H: 22° L:14°</p>
+        <p className="weather-details-container__feels">Feels like {feelsLike} F</p>
+        <p className="weather-details-container__high-low">UVI: {uvi} </p>
       </div>
     </div>
   );
